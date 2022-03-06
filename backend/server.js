02f74a5581599f7bb781bc47d3a9de65a549034c
Ec da KreatOr <88333318+Macedman@users.noreply.github.com>
2022-03-06 16:51:35 +0800

    Controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.use(errorHandler)
-
+ 
 app.listen(port, () => console.log(`Server started on port: ${port}`))
